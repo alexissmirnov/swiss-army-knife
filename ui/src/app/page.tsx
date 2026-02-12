e0256a6be542dcbd296c5b382f7660f0a1ca0695
Evan Luo <evan@ohevan.com>
2026-02-12 18:42:39 +0000

    refactor(web): remove unused imports from home page

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,13 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import Header from "@/components/layout/header";
-import LetterGlitch from "@/components/animations/letter-glitch";
-import { ArrowRight } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import { Divider } from "@/components/ui/divider";
-import Footer from "@/components/layout/footer";
 import { authClient } from "@/lib/auth-client";
-import { Separator } from "@/components/ui/separator";
 
 export default async function Home() {
   const session = await authClient.getSession();
